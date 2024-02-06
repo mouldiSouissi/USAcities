b3da198a68463f16fe22e226f7d138ee4e9a3c5c
Mouldi-souissi <souissi13mouldi@gmail.com>
2024-02-06 01:47:27 +0100

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const USAcities = require("./db/USAcities");
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/cities/:state", (req, res) => {
+  try {
+    const filtered = USAcities.filter((row) => row.state === req.params.state);
+    res.send(filtered);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`server is running on port ${PORT}`);
+});
