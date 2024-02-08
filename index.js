09e1514f68866f866e63153096de9f0a1ec7c5b0
Mouldi-souissi <souissi13mouldi@gmail.com>
2024-02-08 14:56:56 +0100

    docker and states

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const cities = require("./db/cities");
+const states = require("./db/states");
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  try {
+    res.send(`<h1>USA states and cities API</h1>`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.get("/states", (req, res) => {
+  try {
+    res.send(states);
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.get("/cities/:state", (req, res) => {
   try {
     const filtered = cities.filter((row) => row.state === req.params.state);
